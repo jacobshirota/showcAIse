d6725391b8ec742aacd0bea96cda5d602e881595
Jacob Shirota <jacobshirota@gmail.com>
2024-12-02 02:40:42 -0800

    Update app.py to use *.prompt files

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ openai_client = openai.OpenAI()
 
 app = Flask(__name__)
 
+def generate(prompt):
+    chat = openai_client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are an HTML document writing machine. You write single HTML documents with any CSS or JavaScript written in-line with the HTML. You take the prompts for websites given by the user and translate them into documents. You also apply any necessary styles or scripts to make the site look aesthetically pleasing, interesting, and interactive."},
+            {"role": "user", "content": prompt}
+        ],
+        stream=False
+    )
+    return f'data: <div>{parse(chat.choices[0].message.content)}\ndata:</div>\n\n'
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -19,15 +31,7 @@ def index():
 @app.route("/ai")
 def ai():
     def eventStream():
-        chat = openai_client.chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                {"role": "system", "content": "You are an HTML document writing machine. You write single HTML documents with any CSS or JavaScript written in-line with the HTML. You take the prompts for websites given by the user and translate them into documents."},
-                {"role": "user", "content": "Write a unique and interactive portfolio website for a Computer Science student named Jacob Shirota who is set to graduate in December of 2024. Jacob has interests in cybersecurity, data sciences, and software engineering. Jacob is the leader of the Naitonal Cyber League team at Biola University and recently led the team to an all-time-best finish of 61st place, with a personal best finish within the top 100 participants. Include a headshot of him located at /static/profile.png"}
-            ],
-            stream=False
-        )
-
-        return f'data: <div>{parse(chat.choices[0].message.content)}\ndata:</div>\n\n'
+        with open('prompts/index.prompt', 'r') as ifile:
+            return generate(ifile.read())
 
     return Response(eventStream(), mimetype='text/event-stream')
