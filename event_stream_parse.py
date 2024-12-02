31ef66255107fca930f974fea66d91b7b337e7af
Jacob Shirota <jacobshirota@gmail.com>
2024-12-02 02:36:38 -0800

    Fix event stream parser

--- event_stream_parse.py
+++ event_stream_parse.py
@@ -1,8 +1,7 @@
 import re
 
 def parse(content):
-    content = re.sub('<!DOCTYPE html>','', content)
-    content = re.sub('<html.+','', content)
-    content = re.sub('</html>','', content)
+    content = re.sub('.*html.*','', content)
+    content = re.sub("'''", '', content)
     content = re.sub('\n', '\ndata: ', content)
     return content
