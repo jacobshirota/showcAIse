e434dd70eb782a11b16563a0b11eb9ed075eb7b3
Jacob Shirota <jacobshirota@gmail.com>
2024-12-02 10:38:53 -0800

    Migrate entirely to .prompt files

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from event_stream_parse import parse
-from flask import Flask, render_template, Response, url_for
+from event_stream_parse import event_streamify, parse
+from flask import Flask, render_template, Response, render_template_string, send_file, send_from_directory, url_for
 
 import openai
 
 from dotenv import load_dotenv
 import os
+
+
+DEBUGGING = False
+
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -13,25 +17,36 @@ openai_client = openai.OpenAI()
 app = Flask(__name__)
 
 def generate(prompt):
+    if DEBUGGING:
+        return 'data: <div>debugging</div>\n\n'
+    
     chat = openai_client.chat.completions.create(
         model="gpt-4o",
         messages=[
-            {"role": "system", "content": "You are an HTML document writing machine. You write single HTML documents with any CSS or JavaScript written in-line with the HTML. You take the prompts for websites given by the user and translate them into documents. You also apply any necessary styles or scripts to make the site look aesthetically pleasing, interesting, and interactive."},
+            {"role": "system", "content": "You are an HTML document writing machine. You write single HTML documents with any CSS or JavaScript written in-line with the HTML. You take the prompts for websites given by the user and translate them into documents. You also apply any necessary styles (including fonts) or scripts to make the site look aesthetically pleasing, interesting, and interactive. You do not reply with anything other than the raw HTML content. Do not include any copyright symbols."},
             {"role": "user", "content": prompt}
         ],
         stream=False
     )
-    return f'data: <div>{parse(chat.choices[0].message.content)}\ndata:</div>\n\n'
+    return event_streamify(render_template_string(parse(chat.choices[0].message.content)))
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/<page>")
+def index(page="index"):
+    return render_template("index.html", page=page)
 
-@app.route("/ai")
-def ai():
+@app.route("/ai/<page>")
+def ai(page="index"):
     def eventStream():
-        with open('prompts/index.prompt', 'r') as ifile:
+        with open(f'prompts/{page}.prompt', 'r') as ifile:
             return generate(ifile.read())
 
     return Response(eventStream(), mimetype='text/event-stream')
+
+
+
+
+@app.route("/prompts/<page>.prompt")
+def display_prompt(page):
+    return send_from_directory('prompts', f'{page}.prompt', mimetype="text/plain")
--- event_stream_parse.py
+++ event_stream_parse.py
@@ -3,5 +3,10 @@ import re
 def parse(content):
     content = re.sub('.*html.*','', content)
     content = re.sub("'''", '', content)
-    content = re.sub('\n', '\ndata: ', content)
+    content = re.sub("```", '', content)
+    content = re.sub( r'src=[\'\"]([\w/]*.[\w]*)[\'\"]' , r'src="{{url_for("static", filename="\1")}}"' ,content)
     return content
+
+def event_streamify(content):
+    content = re.sub('\n', '\ndata: ', content)
+    return f'data: <div>{content}\ndata:</div>\n\n'
